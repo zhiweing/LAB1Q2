1be3d035feff4fb86fa4c44e80686e5a6432ef22
@ngzhiwei <>
2023-11-11 08:20:09 +0800

    Lab test 1(Q1)

---
+++ src/l1q2/L1Q2.java
@@ -0,0 +1,24 @@
+
+package l1q2;
+public class L1Q2 {
+    public static void main(String[] args) {
+        System.out.println("Full name: Ng Zhi Wei");
+        System.out.println("Matric number: 23051966");
+        System.out.println("Address: Universiti Malaya");
+        System.out.println("Email: ngzhiwei03@gmail.com");
+        System.out.println("Contact: 017-5012923");
+        
+        System.out.println("""
+                           Full name: Ng Zhi Wei
+                           Matric number:23051966
+                           Address: Universiti Malaya
+                           Email: ngzhiwei03@gmail.com
+                           Contact: 017-5012923
+                           """);
+        
+        System.out.println("Full name: Ng Zhi Wei\nMatric number:23051966\nAddress: Universiti Malaya\nEmail: ngzhiwei03@gmail.com\nContact: 017-5012923");
+        
+        
+    }
+    
+}
